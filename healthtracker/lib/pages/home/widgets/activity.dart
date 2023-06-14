3879a74ee95d9bf8d41016c0d2fe6c1d05e67e1e
curlytheses <rdas26244@gmail.com>
2023-06-14 19:20:28 +0530

    commit

--- healthtracker/lib/pages/home/widgets/activity.dart
+++ healthtracker/lib/pages/home/widgets/activity.dart
@@ -6,9 +6,69 @@ class RecentActivities extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        width: double.infinity,
-        color: Colors.blue,
+        child: Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 30,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Recent Activities',
+            style: Theme.of(context).textTheme.displayLarge,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) => ActivityItem(),
+            ),
+          )
+        ],
+      ),
+    ));
+  }
+}
+
+class ActivityItem extends StatelessWidget {
+  const ActivityItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Color(0xffe1e1e1),
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        children: [
+          SizedBox(width: 10),
+          Container(
+            padding: EdgeInsets.all(4),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Color(0xffcff2ff),
+            ),
+            height: 35,
+            width: 35,
+            child: Container(
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage('assets/images gym.jpg'),
+                    fit: BoxFit.fill,
+                  )),
+            ),
+          ),
+          SizedBox(width: 20),
+          Text(
+            'Walking',
+            style: TextStyle(fontSize: 12, fontWeight: FontWeight.w900),
+          )
+        ],
       ),
     );
   }
