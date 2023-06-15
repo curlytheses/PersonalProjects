036441057357f6e978967005fb8830fee939589b
curlytheses <rdas26244@gmail.com>
2023-06-15 20:19:40 +0530

    commit

--- healthtracker/lib/pages/home/widgets/activity.dart
+++ healthtracker/lib/pages/home/widgets/activity.dart
@@ -33,72 +33,85 @@ class RecentActivities extends StatelessWidget {
 class ActivityItem extends StatelessWidget {
   const ActivityItem({super.key});
 
+  static const activities = [
+    'Running',
+    'Swimming',
+    'Hiking',
+    'Waliking',
+    'Cycling',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 5),
-      height: 50,
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Color(0xffe1e1e1),
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed('/details');
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 5),
+        height: 50,
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Color(0xffe1e1e1),
+          ),
+          borderRadius: BorderRadius.circular(10),
         ),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        children: [
-          SizedBox(width: 10),
-          Container(
-            padding: EdgeInsets.all(4),
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Color(0xffcff2ff),
-            ),
-            height: 35,
-            width: 35,
-            child: Container(
+        child: Row(
+          children: [
+            SizedBox(width: 10),
+            Container(
+              padding: EdgeInsets.all(4),
               decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage('assets/images gym.jpg'),
-                    fit: BoxFit.fill,
-                  )),
+                shape: BoxShape.circle,
+                color: Color(0xffcff2ff),
+              ),
+              height: 35,
+              width: 35,
+              child: Container(
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage('assets/images gym.jpg'),
+                      fit: BoxFit.fill,
+                    )),
+              ),
             ),
-          ),
-          SizedBox(width: 20),
-          Text(
-            'Walking',
-            style: TextStyle(fontSize: 12, fontWeight: FontWeight.w900),
-          ),
-          Expanded(child: SizedBox()),
-          Icon(
-            Icons.timer,
-            size: 12,
-          ),
-          SizedBox(
-            width: 5,
-          ),
-          Text(
-            '30 min',
-            style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400),
-          ),
-          SizedBox(
-            width: 10,
-          ),
-          Icon(
-            Icons.wb_sunny_outlined,
-            size: 12,
-          ),
-          SizedBox(
-            width: 5,
-          ),
-          Text(
-            '55 kkal',
-            style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400),
-          ),
-          SizedBox(
-            width: 20,
-          )
-        ],
+            SizedBox(width: 20),
+            Text(
+              'Walking',
+              style: TextStyle(fontSize: 12, fontWeight: FontWeight.w900),
+            ),
+            Expanded(child: SizedBox()),
+            Icon(
+              Icons.timer,
+              size: 12,
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Text(
+              '30 min',
+              style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Icon(
+              Icons.wb_sunny_outlined,
+              size: 12,
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Text(
+              '55 kkal',
+              style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400),
+            ),
+            SizedBox(
+              width: 20,
+            )
+          ],
+        ),
       ),
     );
   }
--- healthtracker/lib/pages/home/widgets/current.dart
+++ healthtracker/lib/pages/home/widgets/current.dart
@@ -10,6 +10,12 @@ class CurrentPrograms extends StatefulWidget {
 
 class _CurrentProgramsState extends State<CurrentPrograms> {
   ProgramType active = fitnessPrograms[0].type;
+  void _changeProgram(ProgramType newType) {
+    setState(() {
+      active = newType;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -41,6 +47,7 @@ class _CurrentProgramsState extends State<CurrentPrograms> {
               return Program(
                 program: fitnessPrograms[index],
                 active: fitnessPrograms[index].type == active,
+                onTap: _changeProgram,
               );
             },
             separatorBuilder: (context, index) => SizedBox(
@@ -56,55 +63,65 @@ class _CurrentProgramsState extends State<CurrentPrograms> {
 class Program extends StatelessWidget {
   final FitnessProgram program;
   final bool active;
-  const Program({super.key, required this.program, this.active = false});
+  final Function(ProgramType) onTap;
+  const Program(
+      {super.key,
+      required this.program,
+      this.active = false,
+      required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: 100,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        image: DecorationImage(
-            colorFilter: ColorFilter.mode(
-                active
-                    ? Color(0xff1ebdf8).withOpacity(0.8)
-                    : Colors.white.withOpacity(0.8),
-                BlendMode.lighten),
-            image: program.image,
-            fit: BoxFit.cover),
-      ),
-      alignment: Alignment.bottomLeft,
-      padding: const EdgeInsets.all(15),
-      child: DefaultTextStyle.merge(
-        style: TextStyle(
-          color: active ? Colors.white : Colors.black,
-          fontSize: 10,
-          fontWeight: FontWeight.w500,
+    return GestureDetector(
+      onTap: () {
+        onTap(program.type);
+      },
+      child: Container(
+        height: 100,
+        width: 100,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          image: DecorationImage(
+              colorFilter: ColorFilter.mode(
+                  active
+                      ? Color(0xff1ebdf8).withOpacity(0.8)
+                      : Colors.white.withOpacity(0.8),
+                  BlendMode.lighten),
+              image: program.image,
+              fit: BoxFit.cover),
         ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(program.name),
-            Row(
-              children: [
-                Text(program.cals),
-                SizedBox(
-                  width: 15,
-                ),
-                Icon(
-                  Icons.timer,
-                  size: 10,
-                  color: active ? Colors.white : Colors.black,
-                ),
-                SizedBox(
-                  width: 5,
-                ),
-                Text(program.time),
-              ],
-            )
-          ],
+        alignment: Alignment.bottomLeft,
+        padding: const EdgeInsets.all(15),
+        child: DefaultTextStyle.merge(
+          style: TextStyle(
+            color: active ? Colors.white : Colors.black,
+            fontSize: 10,
+            fontWeight: FontWeight.w500,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(program.name),
+              Row(
+                children: [
+                  Text(program.cals),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Icon(
+                    Icons.timer,
+                    size: 10,
+                    color: active ? Colors.white : Colors.black,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(program.time),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
