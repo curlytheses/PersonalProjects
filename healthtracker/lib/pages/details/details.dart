8c266fe513b5ae439647c43abfad76aae76ddbdd
curlytheses <rdas26244@gmail.com>
2023-06-16 17:29:46 +0530

    commit

--- healthtracker/lib/pages/details/details.dart
+++ healthtracker/lib/pages/details/details.dart
@@ -2,27 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:healthtracker/pages/details/widgets/appbar.dart';
 import 'package:healthtracker/pages/details/widgets/dates.dart';
 import 'package:healthtracker/pages/details/widgets/graph.dart';
-import 'package:healthtracker/pages/details/widgets/info.dart';
-import 'package:healthtracker/pages/details/widgets/stats.dart';
+import 'package:healthtracker/pages/details/widgets/info.dart' hide Stats;
 import 'package:healthtracker/pages/details/widgets/steps.dart';
 import 'package:healthtracker/widgets/bottom_navigation.dart';
 
+import 'widgets/stats.dart';
+
 class DetailsPage extends StatelessWidget {
-  const DetailsPage({super.key});
+  const DetailsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MainAppBar(
-        appBar: AppBar(),
-      ),
+      backgroundColor: Colors.white,
+      appBar: MainAppBar(appBar: AppBar()),
       body: Column(
-        children: [
+        children: const [
           Dates(),
           Steps(),
           Graph(),
           Info(),
-          States(),
+          Divider(height: 30),
+          Stats(),
+          SizedBox(height: 30),
           BottomNavigation(),
         ],
       ),
--- healthtracker/lib/pages/details/widgets/steps.dart
+++ healthtracker/lib/pages/details/widgets/steps.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import '../../../helpers.dart';
 
 class Steps extends StatelessWidget {
-  const Steps({super.key});
+  const Steps({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 70,
-      color: Colors.yellow,
+    String steps = formatNumber(randBetween(3000, 6000));
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Column(
+        children: [
+          Text(
+            steps,
+            style: const TextStyle(
+              fontSize: 33,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+          const Text(
+            'Total Steps',
+            style: TextStyle(
+              fontSize: 11,
+              fontWeight: FontWeight.w500,
+              height: 2,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
