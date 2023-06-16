8605e5ef070f2702760378dc6ede11150b94932b
curlytheses <rdas26244@gmail.com>
2023-06-16 13:27:00 +0530

    commit

--- healthtracker/lib/pages/details/widgets/dates.dart
+++ healthtracker/lib/pages/details/widgets/dates.dart
@@ -5,10 +5,65 @@ class Dates extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Row(
+      children: [
+        DateBox(),
+        DateBox(
+          active: true,
+        ),
+      ],
+    );
+  }
+}
+
+class DateBox extends StatelessWidget {
+  final bool active;
+  const DateBox({
+    this.active = false,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      height: 90,
-      color: Colors.green,
+      width: 50,
+      height: 70,
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+      decoration: BoxDecoration(
+        gradient: active
+            ? LinearGradient(
+                colors: [
+                  Color(0xff92e2ff),
+                  Color(0xff1ebdf8),
+                ],
+              )
+            : null,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          color: Color(0xffe1e1e1),
+        ),
+      ),
+      child: Column(
+        children: [
+          Text(
+            'Mon',
+            style: TextStyle(
+              fontSize: 10,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            '8',
+            style: TextStyle(
+              fontSize: 27,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
