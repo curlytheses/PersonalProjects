d9cc07375b653698ae045a3f185de34d16f99a0a
curlytheses <rdas26244@gmail.com>
2023-06-17 18:33:05 +0530

    commit

--- healthtracker/lib/pages/workouttimetable/Widgets/workoutlists.dart
+++ healthtracker/lib/pages/workouttimetable/Widgets/workoutlists.dart
@@ -26,7 +26,7 @@ class WorkoutProgramsState extends State<WorkoutPrograms> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
-                'Current Programs',
+                'Current Workouts',
                 style: Theme.of(context).textTheme.displayLarge,
               ),
               Icon(
